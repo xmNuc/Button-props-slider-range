20c60c3f732d044114b2d548d2c723bb0aed3546
xmNuc <xmnuc@o2.pl>
2022-03-26 23:50:06 +0100

    bug fixes

--- src/components/Sliderbtn.js
+++ src/components/Sliderbtn.js
@@ -5,7 +5,6 @@ import { Button } from './Button';
 export const Slider = (props) => {
   const [currVal, setCurrVal] = useState(15);
 
-  // console.log(currVal);
   const handleChange = (e) => {
     setCurrVal(e.target.value);
   };
@@ -21,48 +20,3 @@ export const Slider = (props) => {
     </>
   );
 };
-
-// import React, { useState } from 'react';
-// import './Button.css';
-
-// export const Button = (props) => {
-//   const [currVal, setCurrVal] = useState(15);
-
-//   console.log(currVal);
-//   const handleChange = (e) => {
-//     setCurrVal(e.target.value);
-//   };
-//   return (
-//     <>
-//       <div className="Wrap">
-//         <button className="btn-grad" style={{ padding: props.padding }}>
-//           {props.children}
-//         </button>
-//       </div>
-//       <div>
-//         <input type="range" min="0" max="30" step="1" value={currVal} onChange={handleChange} />
-//       </div>
-//     </>
-//   );
-// };
-
-// import React, { useState } from 'react';
-// import { Button } from './Button';
-
-// export const ButtonXl = (props) => {
-//   const [currVal, setCurrVal] = useState(15);
-
-//   console.log(currVal);
-//   const handleChange = (e) => {
-//     setCurrVal(e.target.value);
-//   };
-
-//   return (
-//     <Button padding={props.padding}>
-//       {props.children}
-//       <div>
-//         <input type="range" min="0" max="30" step="1" value={currVal} onChange={handleChange} />
-//       </div>
-//     </Button>
-//   );
-// };
